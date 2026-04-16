e01ddf9c4b603ce1caeade9278d0552ba9907d5e
kangziyao <805941478@qq.com>
2026-04-16 20:32:17 +0800

    rollback init

--- src/data/sampleData.ts
+++
@@ -1,9 +0,0 @@
-import type { ChartDatum } from "../types/chart";
-
-export const sampleBarData: ChartDatum[] = [
-  { category: "A", value: 30 },
-  { category: "B", value: 52 },
-  { category: "C", value: 41 },
-  { category: "D", value: 67 },
-  { category: "E", value: 58 },
-];
